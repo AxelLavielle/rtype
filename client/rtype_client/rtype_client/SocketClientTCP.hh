5ca74273b456c896e24bd55095af72003a346977
Mohamad Moutaliev <mohamad.moutaliev@epitech.eu>
2016-12-12 13:49:19 +0100

    Socket Client : Adaptation sur Linux

--- client/rtype_client/rtype_client/SocketClientTCP.hh
+++ client/rtype_client/rtype_client/SocketClientTCP.hh
@@ -1,29 +1,41 @@
 #pragma once
 
-#define DEFAULT_BUFLEN 512
+# define DEFAULT_BUFLEN 512
 
-#include <winsock2.h>
-#include <windows.h>
-#include <ws2tcpip.h>
-#include <iphlpapi.h>
+#ifdef _WIN32
+	# include <winsock2.h>
+	# include <windows.h>
+	# include <ws2tcpip.h>
+	# include <iphlpapi.h>
+#elif __linux__
+	# include <stdlib.h>
+	# include <sys/socket.h>
+	# include <arpa/inet.h>
+#endif
+
 #include "ASocketClient.hh"
 
-class SocketClientTCP : public ASocketClient
+class					SocketClientTCP : public ASocketClient
 {
 public:
 	SocketClientTCP();
 	virtual ~SocketClientTCP();
 
-	virtual bool	init(const std::string &, int);
-	virtual bool	sendData(const char *);
-	virtual char	*receiveData();
-	virtual bool	close();
-	virtual bool	connectToServer();
+	virtual bool		init(const std::string &, int);
+	virtual bool		sendData(const char *);
+	virtual char		*receiveData();
+	virtual bool		close();
+	virtual bool		connectToServer();
 
 private:
-	WSADATA			_wsaData;
-	SOCKET			_connectSocket;
-	struct addrinfo	*_result;
-	struct addrinfo *_ptr;
+#ifdef _WIN32
+	WSADATA				_wsaData;
+	SOCKET				_connectSocket;
+	struct addrinfo		*_result;
+	struct addrinfo		*_ptr;
+#elif __linux__
+	int					_sock;
+	struct sockaddr_in	_server;
+#endif
 };
 

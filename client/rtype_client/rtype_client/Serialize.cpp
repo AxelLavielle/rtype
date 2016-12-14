c2b28960f66d3d56d77fcd733cdf445d9f953086
Axel Lavielle <axel.lavielle@epitech.eu>
2016-12-14 05:08:16 +0100

    Serialisation + une désérialisation ok

--- client/rtype_client/rtype_client/Serialize.cpp
+++ client/rtype_client/rtype_client/Serialize.cpp
@@ -48,34 +48,51 @@ ICommand	*Serialize::unserializeInputCmd(char *cmd)
 
 char		*Serialize::serialize(ICommand *cmd)
 {
-  packet	*p;
+  packet	p;
   std::string	tmp;
   int		i;
   char		*ret;
 
   ret = new char[65471];
-  p = new packet[1];
-  p->dataType = cmd->getCommandName();
+  p.dataType = cmd->getCommandName();
   tmp = cmd->getCommandArg();
   i = -1;
   while (tmp[++i] != 0)
-    p->data[i] = tmp[i];
-  p->data[i] = 0;
-  p->dataLength = tmp.size() + 4;
+    p.data[i] = tmp[i];
+  p.data[i] = 0;
+  p.dataLength = tmp.size() + 4;
   i = -1;
-  while (++i != p->dataLength)
-    ret[i] = reinterpret_cast<char *>(p)[i];
+  while (++i != p.dataLength)
+    ret[i] = reinterpret_cast<char *>(&p)[i];
   ret[i] = 0;
-  delete(p);
   return (ret);
 }
 
 IEntity		*Serialize::unserializeEntity(char *data)
 {
+  (void)data;
   return (NULL);
 }
 
+#include	<iostream>
 ICommand	*Serialize::unserializeCommand(char *data)
 {
+  packet	p;
+
+  p = *reinterpret_cast<packet*>(data);
+  std::cout << p.data << std::endl;
+  switch (p.dataType)
+    {
+    case CHATINFO:
+      break;
+    case ROOMINFO:
+      break;
+    case ROOMLIST:
+      break;
+    case ENTITY:
+      break;
+    case INPUT:
+      break;
+    }
   return (NULL);
 }

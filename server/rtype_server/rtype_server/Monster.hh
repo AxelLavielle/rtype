c192204746501934602afdcbda8d2223b417ebe3
Alexis Giordan <alexis.giordan@epitech.eu>
2016-12-15 17:32:45 +0100

    correction architecture Entity

--- server/rtype_server/rtype_server/Monster.hh
+++ server/rtype_server/rtype_server/Monster.hh
@@ -5,15 +5,16 @@
 class Monster :
   public AEntity
 {
-public:
-  Monster();
-  virtual ~Monster();
-  virtual void		setId(const int);
-  virtual int		getId(void) const;
-  virtual void		setName(const std::string &);
-  virtual std::string	getName(void) const;
-  virtual void		update(void);
 protected:
   int			_id;
   std::string		_name;
+public:
+  Monster();
+  virtual ~Monster();
+
+  virtual void		setName(const std::string &);
+  virtual std::string	getName() const;
+  virtual void		update();
+  void		setId(const int);
+  int		getId() const;
 };

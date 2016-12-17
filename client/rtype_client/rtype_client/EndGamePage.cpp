254d2894682cd3f94f77177bc383b8375a197539
Mohamad Moutaliev <mohamad.moutaliev@epitech.eu>
2016-12-17 15:52:14 +0100

    EndPage terminé

--- client/rtype_client/rtype_client/EndGamePage.cpp
+++ client/rtype_client/rtype_client/EndGamePage.cpp
@@ -5,6 +5,10 @@ EndGamePage::EndGamePage(IGraphManager *graph, IEventManager *event, const PathF
 	_backgroundSprite = _fileManager.getRoot() + "/res/img/background_menu3.jpg";
 	_clickSound.setDuration(-1);
 	_clickSound.setFilePath(_fileManager.getRoot() + "/res/sounds/buttonClick.wav");
+	_scores.push_back(std::make_pair<std::string, int>("Kebabaman", 684684));
+	_scores.push_back(std::make_pair<std::string, int>("hmardingdingdong", 453738));
+	_scores.push_back(std::make_pair<std::string, int>("hihijaiungroszizi", 98384));
+	_scores.push_back(std::make_pair<std::string, int>("alexislepd", 12));
 }
 
 EndGamePage::~EndGamePage()
@@ -13,14 +17,15 @@ EndGamePage::~EndGamePage()
 
 bool			EndGamePage::init()
 {
-	initButton(70, 10, 60, Rect(380, 550, 90, 310), "RETURN TO MENU", "/res/img/button.png", "/res/img/buttonOver.png", "/res/fonts/Aerospace.ttf", IPage::HOME);
-	initButton(70, 10, 60, Rect(380, 550, 90, 310), "QUIT", "/res/img/button.png", "/res/img/buttonOver.png", "/res/fonts/Aerospace.ttf", IPage::QUIT);
+	initButton(70, 5, 30, Rect(180, 550, 90, 310), "RETURN\nTO MENU", "/res/img/button.png", "/res/img/buttonOver.png", "/res/fonts/Aerospace.ttf", IPage::HOME);
+	initButton(70, 5, 60, Rect(580, 550, 90, 310), "QUIT", "/res/img/button.png", "/res/img/buttonOver.png", "/res/fonts/Aerospace.ttf", IPage::QUIT);
 	return (true);
 }
 
 void			EndGamePage::clear()
 {
-
+	clearGUIElement(_buttons);
+	clearGUIElement(_guiElement);
 }
 
 bool			EndGamePage::launch()
@@ -30,10 +35,31 @@ bool			EndGamePage::launch()
 
 void			EndGamePage::draw()
 {
+	std::vector< std::pair<std::string, int>>::iterator	it = _scores.begin();
+	int			y = 270;
 
+	_graph->setBackground(_backgroundSprite, 0.6f, 0.7f);
+	initDecor(Rect(290, 200, 50, 500), "/res/img/buttonRoom.png");
+	initDecor(Rect(290, 260, 190, 500), "/res/img/buttonRoom.png");
+	drawGUIElement(_buttons);
+	drawGUIElement(_guiElement);
+	_graph->drawText("GAME OVER", 230, 0, 90, Color(135, 206, 250, 255), _fileManager.getRoot() + "/res/fonts/Aerospace.ttf");
+	_graph->drawText("Score : ", 300, 200, 30, Color(135, 206, 250, 255), _fileManager.getRoot() + "/res/fonts/Aerospace.ttf");
+	while (it != _scores.end())
+	{
+		_graph->drawText(it->first + " : " + std::to_string(it->second), 300, y, 25, Color(135, 206, 250, 255), _fileManager.getRoot() + "/res/fonts/Aerospace.ttf");
+		y += 40;
+		it++;
+	}
 }
 
 IPage::PAGE		EndGamePage::event()
 {
-	return (IPage::ENDGAME);
+	IPage::PAGE		page;
+
+	hoverEvent(_buttons);
+	page = clickEvent(_buttons);
+	if (page != IPage::NONE)
+		_soundManager->play(_clickSound);
+	return (page);
 }
--- client/rtype_client/rtype_client/EndGamePage.hh
+++ client/rtype_client/rtype_client/EndGamePage.hh
@@ -2,18 +2,19 @@
 
 #include "APage.hh"
 
-class						EndGamePage : public APage
+class								EndGamePage : public APage
 {
 public:
 	EndGamePage(IGraphManager *, IEventManager *, const PathFileManager &, ISoundManager *);
 	virtual ~EndGamePage();
 
-	virtual bool			init();
-	virtual void			clear();
-	virtual bool			launch();
-	virtual void			draw();
-	virtual IPage::PAGE		event();
+	virtual bool					init();
+	virtual void					clear();
+	virtual bool					launch();
+	virtual void					draw();
+	virtual IPage::PAGE				event();
 
 private:
-	Sound					_clickSound;
+	Sound							_clickSound;
+	std::vector < std::pair<std::string, int>> _scores;
 };

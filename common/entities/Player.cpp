55e1c960475f9e1871e6216a10525bc7cb4ec78a
Valerian Polizzi <valerian.polizzi@epitech.eu>
2016-12-19 16:21:39 +0000

    ajout dossier entitées compilées avec leurs sources (.so & .lib)

---
+++ common/entities/Player.cpp
@@ -0,0 +1,27 @@
+#include "Player.hh"
+#include "EntityType.hh"
+#include <iostream>
+
+Player::Player()
+{
+  this->setType(rtype::PLAYER);
+  this->setName("Player1");
+}
+
+Player::~Player()
+{
+}
+
+int		Player::getId(void) const
+{
+  return (_id);
+}
+
+void		Player::setId(const int id)
+{
+  _id = id;
+}
+
+void		Player::update(void)
+{
+}

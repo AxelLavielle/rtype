160ecdc1283203c1ba7a0e60474306f58f2ba014
Anthony Vogelweid <anthony.vogelweid@epitech.eu>
2016-12-19 16:52:24 +0100

    correction erreur de merge

--- client/rtype_client/rtype_client/ASocketClient.hh
+++ client/rtype_client/rtype_client/ASocketClient.hh
@@ -17,11 +17,7 @@ public:
 	virtual bool	sendData(const char *, const int datasize) = 0;
 	virtual char	*receiveData() = 0;
 	virtual bool	connectToServer() = 0;
-<<<<<<< HEAD
-	virtual bool	closure() = 0;
 	virtual bool	isConnected() const;
-=======
->>>>>>> 6279344c6ec8198217d80640f8903b8d820c7d66
 
 protected:
 	int				_port;

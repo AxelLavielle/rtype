db3d1440df52667f049f01560848b50de2b237ba
Alexis Giordan <alexis.giordan@epitech.eu>
2016-12-29 18:16:48 +0100

    correction architecture

--- client/rtype_client/rtype_client/CmdManager.cpp
+++ client/rtype_client/rtype_client/CmdManager.cpp
@@ -323,7 +323,7 @@ bool		CmdManager::sendUDPCmd()
 			std::cerr << "ERROR: cant not send data" << std::endl;
 			return (false);
 		}
-		delete (res);
+		delete[] res;
 		delete (*it);
 		it = _cmd.erase(it);
 	}

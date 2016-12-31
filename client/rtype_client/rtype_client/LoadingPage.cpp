c2f8f47dbae2963d33d2efcc76f65fb26e9647da
Mohamad Moutaliev <mohamad.moutaliev@epitech.eu>
2016-12-31 20:28:28 +0100

    loading page modifiable

--- client/rtype_client/rtype_client/LoadingPage.cpp
+++ client/rtype_client/rtype_client/LoadingPage.cpp
@@ -11,16 +11,10 @@ LoadingPage::~LoadingPage()
 bool LoadingPage::init()
 {
 	initDecor(Rect(_windowSize.first / 2 - 200, _windowSize.second / 2 - 100, 200, 400), "/res/img/buttonRoom.png");
-	//initButton(70, 5, 60, Rect(_windowSize.first / 2 - 150, _windowSize.second / 2 - 60, 30, 70), "", "/res/img/spaceShip10.png", "/res/img/spaceShip10.png", "/res/fonts/Space.ttf");
-	//initButton(70, 5, 60, Rect(_windowSize.first / 2 - 150, _windowSize.second / 2 + 30, 30, 70), "", "/res/img/spaceShip10.png", "/res/img/spaceShip10.png", "/res/fonts/Space.ttf");
 	_animInc = _windowSize.first / 2 - 200;
 	_animInc2 = _windowSize.first / 2 - 35;
-//	_animInc3 = _windowSize.first / 2 - 200;
-//	_animInc4 = _windowSize.first / 2;
-	//_imgW = 70;
-	//_img2W = 70;
-//	_img3W = 0;
-//	_img4W = 0;
+	_text = "Loading ...";
+	_textPos = 120;
 	return (true);
 }
 
@@ -61,7 +55,7 @@ void LoadingPage::draw()
 	loadingAnim();
 	drawGUIElement(_guiElement);
 	drawGUIElement(_buttons);
-	_graph->drawText("Loading...", _windowSize.first / 2 - 80, _windowSize.second / 2 - 20, 30, Color(135, 206, 250, 255), _fileManager.getRoot() + "/res/fonts/Space.ttf");
+	_graph->drawText(_text, _windowSize.first / 2 - 200 + _textPos, _windowSize.second / 2 - 20, 30, Color(135, 206, 250, 255), _fileManager.getRoot() + "/res/fonts/Space.ttf");
 	_graph->drawRectangle(_fileManager.getRoot() + "/res/img/spaceShip10.png", Rect(_animInc, _windowSize.second / 2 - 60, 0, 0), Rect(0, 0, 0, 0), Rect(0, 0, 30, 70));
 	_graph->drawRectangle(_fileManager.getRoot() + "/res/img/spaceShip10.png", Rect(_animInc2, _windowSize.second / 2 + 30, 0, 0), Rect(0, 0, 0, 0), Rect(0, 0, 30, 70));
 }
@@ -73,4 +67,14 @@ IPage::PAGE LoadingPage::event()
 	hoverEvent(_buttons);
 	page = clickEvent(_buttons);
 	return (page);
+}
+
+void		LoadingPage::setText(const std::string &text)
+{
+	_text = text;
+}
+
+void		LoadingPage::setTextPos(const int textPos)
+{
+	_textPos = textPos;
 }

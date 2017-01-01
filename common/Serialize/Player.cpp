24e16697175d834cac708f3a7f6310e7be2d8bac
Sarah TACCONELLI <sarah.tacconelli@epitech.eu>
2017-01-01 02:25:56 +0100

    Chemin Sprite Player modifié

--- common/Serialize/Player.cpp
+++ common/Serialize/Player.cpp
@@ -11,7 +11,7 @@ Player::Player(const int x, const int y, const int idPlayer)
   this->setSpeedX(1);
   this->setSpeedY(1);
   this->setLife(100);
-  this->setSpriteRepo("/res/img/spaceShip10.png");
+  this->setSpriteRepo("/res/img/ship/spaceShip10.png");
   this->_collisionBox = new CollisionBox(this);
   refresh();
   this->_missileCooldown = 0;

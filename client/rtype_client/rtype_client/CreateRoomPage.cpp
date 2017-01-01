9b5b460ce49099cb85baed200f62ec1f8ab18973
Mohamad Moutaliev <mohamad.moutaliev@epitech.eu>
2017-01-01 15:19:50 +0100

    CreateRoomPage peut maintenant prendre le nombre de vague et la seed

--- client/rtype_client/rtype_client/CreateRoomPage.cpp
+++ client/rtype_client/rtype_client/CreateRoomPage.cpp
@@ -16,7 +16,9 @@ bool CreateRoomPage::init()
 {
 	initButton(70, 5, 60, Rect(_windowSize.first / 2 - 155 + _windowSize.first / 4, 900, 90, 310), "PLAY", "/res/img/button.png", "/res/img/buttonOver.png", "/res/fonts/Aerospace.ttf", IPage::CREATEROOMACTION);
 	initButton(70, 5, 60, Rect(_windowSize.first / 2 - 155 - _windowSize.first / 4, 900, 90, 310), "BACK", "/res/img/button.png", "/res/img/buttonOver.png", "/res/fonts/Aerospace.ttf", IPage::PLAY);
+	initInputBox(Rect(_windowSize.first / 2 - 350, 350, 30, 700), "/res/img/buttonRoom.png", Color(255, 255, 255));
 	initInputBox(Rect(_windowSize.first / 2 - 350, 500, 30, 700), "/res/img/buttonRoom.png", Color(255, 255, 255));
+	initInputBox(Rect(_windowSize.first / 2 - 350, 650, 30, 700), "/res/img/buttonRoom.png", Color(255, 255, 255));
 	return (true);
 }
 
@@ -35,6 +37,9 @@ void CreateRoomPage::draw()
 {
 	_graph->setBackground(_backgroundSprite, 1.0f, 1.1f);
 	_graph->drawText("Hen Type", _windowSize.first / 2 - 250, 100, 90, Color(135, 206, 250, 255), _fileManager.getRoot() + "/res/fonts/Aerospace.ttf");
+	_graph->drawText("Room name", _windowSize.first / 2 - 90, 300, 30, Color(135, 206, 250, 255), _fileManager.getRoot() + "/res/fonts/Aerospace.ttf");
+	_graph->drawText("Waves", _windowSize.first / 2 - 60, 450, 30, Color(135, 206, 250, 255), _fileManager.getRoot() + "/res/fonts/Aerospace.ttf");
+	_graph->drawText("Seed", _windowSize.first / 2 - 50, 600, 30, Color(135, 206, 250, 255), _fileManager.getRoot() + "/res/fonts/Aerospace.ttf");
 	drawGUIElement(_buttons);
 	drawGUIElement(_guiElement);
 }
@@ -78,4 +83,52 @@ std::string CreateRoomPage::getRoomName() const
 		++it;
 	}
 	return ("");
+}
+
+int			CreateRoomPage::getWaves() const
+{
+	std::vector<AGUIElement* >::const_iterator	it;
+	int i = 0;
+
+	it = _guiElement.begin();
+	while (it != _guiElement.end())
+	{
+		if ((*it)->getTypeName() == "InputBox")
+			i++;
+		if ((*it)->getTypeName() == "InputBox" && i == 2)
+		{
+			InputBox	*box;
+
+			box = static_cast<InputBox* >((*it));
+			if (box->getText().size() == 0)
+				return (0);
+			return (std::stoi(box->getText()));
+		}
+		++it;
+	}
+	return (0);
+}
+
+int			CreateRoomPage::getSeed() const
+{
+	std::vector<AGUIElement* >::const_iterator	it;
+	int i = 0;
+
+	it = _guiElement.begin();
+	while (it != _guiElement.end())
+	{
+		if ((*it)->getTypeName() == "InputBox")
+			i++;
+		if ((*it)->getTypeName() == "InputBox" && i == 3)
+		{
+			InputBox	*box;
+
+			box = static_cast<InputBox* >((*it));
+			if (box->getText().size() == 0)
+				return (0);
+			return (std::stoi(box->getText()));
+		}
+		++it;
+	}
+	return (0);
 }
